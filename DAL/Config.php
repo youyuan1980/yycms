947509f0c31108c990f136ea12d0aee449bb56dd
youyuan1980 <fun07750822@sina.com>
2016-02-18 10:11:55 +0800

    20160218

--- DAL/Config.php
+++ DAL/Config.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 15-4-12
- * Time: 下午1:36
- */
 include_once('mysqldb.php');
 $config=array(
     "Server"=>"localhost", //连接服务器名
-    "UserID"=>"yycms",     //用户名
-    "UserPwd"=>"123",     //密码
+    "UserID"=>"root",     //用户名
+    "UserPwd"=>"123456",     //密码
     "DataBase"=>"yycms"   //数据库名
 );
 $DB = new MySqlDB($config);
